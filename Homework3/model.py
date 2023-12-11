f03810673921310d2352b8867761d03da0f13498
Kendric Hood <khood5@kent.edu>
2023-12-10 20:21:58 -0500

    training on macos

--- Homework3/model.py
+++ Homework3/model.py
@@ -14,31 +14,56 @@ class textInputEmbedding(nn.Module):
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.size_of_embedding) #  math.sqrt helps with the convergence 
     
-class positionEncoding(nn.Module):
-    def __init__(self, sizeOfEmbedding:int, sectanceLength: int, dropout: float, scale=10000.0) -> None:
-        super().__init__()
-        self.size_of_embedding = sizeOfEmbedding
-        self.length_of_sectance = sectanceLength
-        self.dropout = nn.Dropout(dropout)
-        self.position_encoding_scaling = scale
+# class positionEncoding(nn.Module):
+#     def __init__(self, sizeOfEmbedding:int, sectanceLength: int, dropout: float, scale=10000.0) -> None:
+#         super().__init__()
+#         self.size_of_embedding = sizeOfEmbedding
+#         self.length_of_sectance = sectanceLength
+#         self.dropout = nn.Dropout(dropout)
+#         self.position_encoding_scaling = scale
 
-        # create matrix for each postion encoding (length_of_sectance * size_of_embedding)
-        positionEncodingMatrix = torch.zeros(self.length_of_sectance, self.size_of_embedding)
-        aPosition = torch.arange(0, self.length_of_sectance, dtype=torch.float32).unsqueeze(1)
-        # The value 10000 is chosen arbitrarily, it is a hyperparameter that can be tuned to scale the position encoding
-        div = torch.exp(torch.arange(0, self.size_of_embedding, 2).float() * (-math.log(self.position_encoding_scaling) / self.size_of_embedding))
+#         # create matrix for each postion encoding (length_of_sectance * size_of_embedding)
+#         positionEncodingMatrix = torch.zeros(self.length_of_sectance, self.size_of_embedding)
+#         aPosition = torch.arange(0, self.length_of_sectance, dtype=torch.float32).unsqueeze(1)
+#         # The value 10000 is chosen arbitrarily, it is a hyperparameter that can be tuned to scale the position encoding
+#         div = torch.exp(torch.arange(0, self.size_of_embedding, 2).float() * (-math.log(self.position_encoding_scaling) / self.size_of_embedding))
         
-        positionEncodingMatrix[:, 0::2] = torch.sin(aPosition*div)
-        positionEncodingMatrix[:, 1::2] = torch.cos(aPosition*div)
+#         positionEncodingMatrix[:, 0::2] = torch.sin(aPosition*div)
+#         positionEncodingMatrix[:, 1::2] = torch.cos(aPosition*div)
 
-        self.positionEncodingMatrix = positionEncodingMatrix.unsqueeze(0)
+#         self.positionEncodingMatrix = positionEncodingMatrix.unsqueeze(0)
 
-        # self.register_buffer('positionEncodingMatrix', positionEncodingMatrix)
-        self.register_buffer('pe', positionEncodingMatrix)
-    def forward(self, x):
-        x = x + (self.positionEncodingMatrix[:, :x.shape[1], :]).requires_grad_(False)
-        return self.dropout(x)
+#         # self.register_buffer('positionEncodingMatrix', positionEncodingMatrix)
+#         self.register_buffer('pe', positionEncodingMatrix)
+#     def forward(self, x):
+#         x = x + (self.positionEncodingMatrix[:, :x.shape[1], :]).requires_grad_(False)
+#         return self.dropout(x)
     
+class positionEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
+        # Apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        return self.dropout(x)
  
 class addNormLayer(nn.Module):
     # 10**-6 is used to just match with tuoral so testing should be the similar 
@@ -96,7 +121,7 @@ class multiHeadAttentionLayer(nn.Module):
         value = value.view(value.shape[0], value.shape[1], self.number_of_heads, self.size_of_individual_q_k_v).transpose(1,2)
         
         x, self.attention_scores = multiHeadAttentionLayer.attention(query, key, value, mask, self.dropout)
-        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.sizeOfEmbedding)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.size_of_embedding)
         x = self.wights_output(x)
         return x
 
@@ -107,37 +132,34 @@ class multiHeadAttentionLayer(nn.Module):
         d_k = query.shape[-1]
 
         attention_scores = (query @ key.transpose(-2, -1))/math.sqrt(d_k)
-        if mask: # apply it
+        if mask is not None: # apply it
             attention_scores.masked_fill_(mask == 0, -1e9) # -1e9 borrowed from tutroal 
         attention_scores = attention_scores.softmax(dim=-1) 
         
-        if dropout: # apply it
+        if dropout is not None: # apply it
             attention_scores = dropout(attention_scores)
 
         return (attention_scores @ value), attention_scores # last value used for viz not needed for model
 
 
 class residualConnection(nn.Module):
-    def __init__(self, dropout:float ) -> None:
+    def __init__(self, features: int, dropout:float ) -> None:
         super().__init__()
         self.dropout = nn.Dropout(dropout)
-        self.norm = addNormLayer()
+        self.norm = addNormLayer(features)
 
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
     
 class encoderLayer(nn.Module):
-    def __init__(self, selfAttentionBlock: multiHeadAttentionLayer, ffBlock: ffLayer, dropout: float) -> None:
+    def __init__(self, features: int, selfAttentionBlock: multiHeadAttentionLayer, ffBlock: ffLayer, dropout: float) -> None:
         super().__init__()
         self.self_attention_block = selfAttentionBlock
         self.ff_block = ffBlock
-        self.residual_connections = nn.ModuleList([residualConnection(dropout) for _ in range(2)])
-
-    def selfAttention(self, x, src_mask):
-        return self.self_attention_block(x, x, x, src_mask)
+        self.residual_connections = nn.ModuleList([residualConnection(features, dropout) for _ in range(2)])
 
     def forward(self, x, src_mask):
-        x = self.residual_connections[0](x, self.selfAttention(x, src_mask))
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
         x = self.residual_connections[1](x, self.ff_block)
         return x
 
@@ -155,31 +177,30 @@ class encoder(nn.Module):
 
 
 class decoderLayer(nn.Module):
-    def __init__(self, selfAttentionBlock: multiHeadAttentionLayer, corssAttentionBlock: multiHeadAttentionLayer, 
+    def __init__(self, features: int, selfAttentionBlock: multiHeadAttentionLayer, corssAttentionBlock: multiHeadAttentionLayer, 
                  ffBlock: ffLayer, dropout:float) -> None:
         super().__init__()
         self.self_attention_block = selfAttentionBlock
         self.corss_attention_block = corssAttentionBlock
         self.ff_block = ffBlock
 
-        self.residual_connections = nn.ModuleList([residualConnection(dropout) for _ in range(3)])
+        self.residual_connections = nn.ModuleList([residualConnection(features, dropout) for _ in range(3)])
 
-    def selfAttention(self, x, encoderOutput, mask):
-        return self.self_attention_block(x, encoderOutput, encoderOutput, mask)
 
     def forward(self, x, encoderOutput, srcMask, targetMask):
-        x = self.residual_connections[0](x, self.selfAttention(x, x, targetMask))
-        x = self.residual_connections[1](x, self.selfAttention(x, encoderOutput, srcMask))
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, srcMask))
+        x = self.residual_connections[1](x, lambda x: self.corss_attention_block(x, encoderOutput, encoderOutput, srcMask))
         x = self.residual_connections[2](x, self.ff_block)
+        return x
 
 class decoder(nn.Module):
     def __init__(self, layers: nn.ModuleList) -> None:
         super().__init__()
-        self.layer = layers
+        self.layers = layers
         self.norm= addNormLayer()
 
     def forward(self, x, encoderOutput, srcMask, targetMask):
-        for layer in self.layer:
+        for layer in self.layers:
             x = layer(x, encoderOutput, srcMask, targetMask)
         return self.norm(x)
 
@@ -194,13 +215,13 @@ class projectionLayer(nn.Module):
         return torch.log_softmax(self.projection(x), dim=-1)
 
 class transformer(nn.Module):
-    def __init__(self, encoder: encoder, decoder: decoder, 
+    def __init__(self, Encoder: encoder, Decoder: decoder, 
                  srcLangEmbedding: textInputEmbedding, targetLangEmbedding: textInputEmbedding,
                  srcPos: positionEncoding, targetPos: positionEncoding,
                  projLayer: projectionLayer) -> None:
         super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
+        self.Encoder = Encoder
+        self.Decoder = Decoder
         self.src_lang_embedding = srcLangEmbedding
         self.target_lang_embedding = targetLangEmbedding
         self.src_pos = srcPos
@@ -210,12 +231,12 @@ class transformer(nn.Module):
     def encode(self, src, src_mask):
         src = self.src_lang_embedding(src)
         src = self.src_pos(src)
-        return self.encoder(src, src_mask)
+        return self.Encoder(src, src_mask)
 
     def decode(self, encoderOutput, srcMask, target, targetMask):
         target = self.target_lang_embedding(target)
         target = self.target_pos(target)
-        return self.decoder(target, encoderOutput, srcMask, targetMask)
+        return self.Decoder(target, encoderOutput, srcMask, targetMask)
     
     def project(self, x):
         return self.proj_layer(x)
@@ -236,7 +257,7 @@ def buildTransformer(srcVocabSize: int, targetVocabSize: int, srcLength:int, tar
     for _ in range(numberOfBlocks):
         encoderSelfAttentionBlock = multiHeadAttentionLayer(sizeOfEmbedding, numberOfHeads, dropout)
         ffBlock = ffLayer(sizeOfEmbedding, ffSize, dropout)
-        encoderBlock = encoderLayer(encoderSelfAttentionBlock, ffBlock, dropout)
+        encoderBlock = encoderLayer(sizeOfEmbedding, encoderSelfAttentionBlock, ffBlock, dropout)
         encoderBlocks.append(encoderBlock)
 
     # make n decoder blocks 
@@ -245,7 +266,7 @@ def buildTransformer(srcVocabSize: int, targetVocabSize: int, srcLength:int, tar
         decoderSelfAttentionBlock = multiHeadAttentionLayer(sizeOfEmbedding, numberOfHeads, dropout)
         decoderCrossAttentionBlock = multiHeadAttentionLayer(sizeOfEmbedding, numberOfHeads, dropout)
         ffBlock = ffLayer(sizeOfEmbedding, ffSize, dropout)
-        decoderBlock = decoderLayer(decoderSelfAttentionBlock, decoderCrossAttentionBlock, ffBlock, dropout)
+        decoderBlock = decoderLayer(sizeOfEmbedding, decoderSelfAttentionBlock, decoderCrossAttentionBlock, ffBlock, dropout)
         decoderBlocks.append(decoderBlock)
 
     # create en/de coder
